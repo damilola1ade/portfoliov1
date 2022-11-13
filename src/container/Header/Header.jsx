118240db78472f53e2245e719be659f5126dadb3
damilola1ade <damiiyi@gmail.com>
2022-11-13 20:52:32 +0100

    Fixed bugs

--- src/container/Header/Header.jsx
+++ src/container/Header/Header.jsx
@@ -28,7 +28,7 @@ const Header = () => (
           <span>👋</span>
           <div style={{ marginLeft: 20 }}>
             <p className="p-text">Hi, I'm</p>
-            <h1 className="head-text">Damilola</h1>
+            <h1 className="head-text">Dami</h1>
           </div>
         </div>
 
